54e918ba56021d3316a0652fc42acbb2219eed22
Christoph1812 <99895259+Christoph1812@users.noreply.github.com>
2023-08-16 16:26:30 +0200

    added slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'christophhaase';
+export class AppComponent implements OnInit {
+  images = ['code.jpg', 'factory.jpg', 'laptop.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 100);
+    }, 8000);
+  }
+
+
+
+
 }
