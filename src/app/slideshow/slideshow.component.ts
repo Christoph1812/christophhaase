d293cd4e2832ccdb70343edf94e2eb03bd4de9ff
Christoph1812 <99895259+Christoph1812@users.noreply.github.com>
2023-08-18 14:36:04 +0200

    added menu.component

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['code.jpg', 'factory.jpg', 'laptop.jpg'];
+  headlines = [
+    'Work in factory',
+    'Born to code',
+    'Bring engineering to the next level '
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 100);
+    }, 8000);
+  }
+
+
+
+}
